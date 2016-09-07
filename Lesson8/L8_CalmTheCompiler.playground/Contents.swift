d382b5d710c8441d19a00df1bb4789610d52fde7
Reinaldo Verdugo <rverdugo@solsteace.com>
2016-09-07 11:55:58 -0400

    Exercises lesson 8

--- Lesson8/L8_CalmTheCompiler.playground/Contents.swift
+++ Lesson8/L8_CalmTheCompiler.playground/Contents.swift
@@ -5,12 +5,29 @@ protocol DirtyDeeds {
     func steal()
 }
 
-class Minion: DirtyDeeds {
+class Minion: DirtyDeeds, Souschef {
     var name: String
     
     init(name:String) {
         self.name =  name
     }
+    
+    func cheat () {
+        print("I am cheating")
+    }
+    
+    func steal () {
+        print("I am stealing")
+    }
+    
+    func chop(vegetable: String) -> String {
+        return "I am chopping \(vegetable)"
+    }
+    
+    func rinse(vegetable: String) -> String {
+        return "I am rinsing \(vegetable) or something"
+    }
+
 }
 
 // Problem 2
@@ -37,12 +54,13 @@ protocol DogWalker {
 
 class Neighbor: DogWalker {
     
-    func throwBall(numberOfTimes:Int) {
+    func throwBall(numberOfTimes:Int) -> Int {
         var count = 0
         while count < numberOfTimes {
             print("Go get it!")
             count += 1
         }
+        return count
     }
     
     func rubBelly() {
